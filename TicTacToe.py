92e93dfa54a4da77d474df1d41d1e2f3c36cb60e
AMAProjects <angelamolinaalhambra@gmail.com>
2024-11-14 17:13:07 +0100

    Creation of board print

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,2 +1,11 @@
-print("Hello, TicTacToe!")
-print('test 2 final')
+#Creating the board
+board = [["  " for i in range(3)] for i in range(3)]
+
+
+def print_board():
+    print('__________')
+    for row in board:
+        print('|'+ '|'.join(row)+'|')
+        print('|'+ ("__|" * 3))
+        
+print_board()
