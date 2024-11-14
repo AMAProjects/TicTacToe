aa47ccd0d1c5593b9ce867596199f9b8f6ba92d3
AMAProjects <angelamolinaalhambra@gmail.com>
2024-11-14 17:47:41 +0100

    adding input commands for player

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,3 +9,16 @@ def print_board():
         print('|'+ ("__|" * 3))
         
 print_board()
+
+#Player Alternation
+current_player = 'X'
+if current_player == 'X':
+    current_player = 'O'
+else:
+    current_player = 'X'
+
+#Rows and collumns input
+row = int(input("Enter row (0, 1, or 2): "))
+column = int(input("Enter column (0, 1, or 2): "))
+
+
