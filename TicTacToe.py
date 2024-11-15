d18250e4aad0feb8aa2b264eadcbe76198f0745a
AMAProjects <angelamolinaalhambra@gmail.com>
2024-11-15 15:47:02 +0100

    Final game changed, debuged the problem with ending the game after check_winner and added error handling for off-limits input. Finished the code loop

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,71 +1,61 @@
-#Creating the board
-board = [["  " for i in range(3)] for i in range(3)]
-
+# Creating the board
+board = [["  " for _ in range(3)] for _ in range(3)]
 
 def print_board():
     print('__________')
     for row in board:
         print('|'+ '|'.join(row)+'|')
         print('|'+ ("__|" * 3))
-        
+
 print_board()
 
-#Player Alternation
+# Player Alternation
 current_player = 'X'
 
-#Rows and columns input
-row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
-column = int(input("Enter column (0, 1, or 2): "))
-
-
-#Changing board from player input
 def play_position(row, column, current_player):
-    if board[row][column] == "  ":
-        board[row][column] = current_player + " " 
+    if 0 <= row < 3 and 0 <= column < 3 and board[row][column] == "  ":
+        board[row][column] = current_player + " "
+        print_board()
     else:
-        print('Oops, this spot is already taken! Try a different one.')
-        row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
-        column = int(input("Enter column (0, 1, or 2): "))
-        board[row][column] = current_player + " " 
-    
-        
-play_position(row, column, current_player)
-print_board()
-
-#Check for winner
-def line_wins(line, current_player):
-    return line[0]==line[1]==line[2] == current_player
-
-def check_winner(current_player):
-    #Checking rows
-    for row in range(3):
-        if line_wins(board[row], current_player):
-            print(f'Player {current_player} won the game!!')
-            return True
-    #Checking columns
-    for column in range(3):
-        if line_wins(board[column], current_player):
-            print(f'Player {current_player} won the game!!')
-            return True
-    #Checking diagonals
-    diagonals = [board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]]
-    for diagonal in diagonals:
-        if line_wins(diagonal,current_player):
-            print(f'Player {current_player} won the game!!')
-            return True
-    return False
-    
-#Keeping the game's loop
-end_game = False
-
-while not end_game:
-    if check_winner(current_player):  # Check for a winner
-        end_game = True 
-        print("Game Over.") # End the game if there is a winner
-    else:
-        # Switch player after each valid turn
-        current_player = 'O' if current_player == 'X' else 'X'
+        print('Oops, this spot is already taken or the input is out of bounds! Try a different one.')
         row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
         column = int(input("Enter column (0, 1, or 2): "))
         play_position(row, column, current_player)
-        print_board()  # Update board
+
+def check_winner(current_player):
+    # Check rows
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2] == current_player + " ":
+            return True
+    # Check columns
+    for column in range(3):
+        if board[0][column] == board[1][column] == board[2][column] == current_player + " ":
+            return True
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] == current_player + " " or \
+       board[0][2] == board[1][1] == board[2][0] == current_player + " ":
+        return True
+    return False
+
+def check_tie():
+    for row in board:
+        if "  " in row:
+            return False
+    return True
+
+def game_loop():
+    global current_player
+    while True:
+        row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
+        column = int(input("Enter column (0, 1, or 2): "))
+        play_position(row, column, current_player)
+        if check_winner(current_player):
+            print(f'Player {current_player} won the game!!')
+            break
+        elif check_tie():
+            print("Game Over, it's a tie!")
+            break
+        else:
+            current_player = 'O' if current_player == 'X' else 'X'
+
+game_loop()
